3a40fab3b673b6ccb6c21b2d5b6b20841e423549
salem2400 <namperone9@gmail.com>
2023-09-04 18:32:59 +0300

    hello im commit from bin dir

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[7:10]
 middle_word = word[1:8]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print(f"First 3 letters: {word_first_3:s}")
+print(f"Last 2 letters: {word_last_2:s}")
+print(f"Middle word: {middle_word:s}")
