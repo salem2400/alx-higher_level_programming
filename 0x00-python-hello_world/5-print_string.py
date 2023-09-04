77df74b33b6aa924eccd088811d29f734d7f384e
salem2400 <namperone9@gmail.com>
2023-09-04 18:05:26 +0300

    hello im commit from bin dir

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"{str[0:10]:s} {str[10:17]:s}{str[0:10]:s} {str[10:17]:s}{str[0:10]:s}{str[10:17]:s} \n{str[0:10]:s}")
+print(3 * str)
+print(str[:9])
