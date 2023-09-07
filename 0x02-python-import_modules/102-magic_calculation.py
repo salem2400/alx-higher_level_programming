1a775160a1f078447b0d0f140a4bb15379347a76
salem2400 <namperone9@gmail.com>
2023-09-08 01:57:39 +0300

    hello im commit from bin dir

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -11,3 +10,4 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
+    return 0
