daf04ad0b56305940eadf5a2a43ab3554a7f9ba1
salem2400 <namperone9@gmail.com>
2023-09-12 02:57:44 +0300

    hello im commit from bin dir

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
