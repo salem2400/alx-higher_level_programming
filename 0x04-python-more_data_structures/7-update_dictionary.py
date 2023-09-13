8af90a099e4e861ac2333e56e1c3f79e29c489b3
salem2400 <namperone9@gmail.com>
2023-09-14 01:42:56 +0300

    hello im commit from bin dir

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
